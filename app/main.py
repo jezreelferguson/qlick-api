29d49c506f7d703b1f09f2f20c201e568df35fae
Ferguson Jezreel <anojafa24@gmail.com>
2026-06-23 12:44:09 +0000

    feat: added cors

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
 from app.database import Base, engine
 from app.routers.auth_router import router
-
-Base.metadata.create_all(bind=engine)
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",      # React
+    "http://localhost:8081",      # Expo development server
+    "exp://192.168.1.100:8081",   # Expo Go (optional)
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+Base.metadata.create_all(bind=engine)
+
 
 app.include_router(router)
 
